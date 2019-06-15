ac2684b3a3dafd29ebc7fc43d52a32b70dc045d4
Takumi Endo <en0605taku@gmail.com>
2019-06-15 17:52:46 +0900

    Finish.

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number:Int = 0
+    @IBOutlet var label:UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+        color(number: number)
+    }
+    
+    @IBAction func mainasu(){
+        number = number - 1
+        label.text = String(number)
+        
+        color(number: number)
+    }
+    
+    @IBAction func kakeru(){
+        number = number * 2
+        label.text = String(number)
+        
+        color(number: number)
+    }
+    
+    @IBAction func waru(){
+        number = number / 2
+        label.text = String(number)
+        
+        color(number: number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        
+        color(number: number)
+    }
+    
+    func color(number :Int){
+        if number >= 10 {
+            label.textColor = UIColor(red: 65/255, green: 105/255, blue: 255/255, alpha: 1.0)
+        }else if number <= -10 {
+             label.textColor = UIColor(red: 178/255, green: 34/255, blue: 34/255, alpha: 1.0)
+        }else{
+            label.textColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1.0)
+        }
+    }
+    
+    
 }
 
